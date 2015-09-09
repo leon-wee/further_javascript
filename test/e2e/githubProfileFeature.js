73f17eb7273ff9c86d67ea2e8202c21f462ed4c1
leon-wee <leonwee27@gmail.com>
2015-09-09 15:13:36 +0700

    Passed 2nd Protractor test

---
+++ test/e2e/githubProfileFeature.js
@@ -0,0 +1,10 @@
+describe('Github Profile finder', function() {
+  it('finds profiles', function() {
+    browser.get('http://localhost:8080');
+
+    element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
+    element(by.className('btn')).click();
+
+    expect(element(by.binding('user.login')).getText()).toEqual('spike01')
+  });
+});
