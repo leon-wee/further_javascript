0ea7497785167088bc0a2008071acaaf6b085661
leon-wee <leonwee27@gmail.com>
2015-09-09 15:50:45 +0700

    Used element all to pass the test

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -14,7 +14,8 @@ describe('Github Profile finder', function() {
   it('finds profiles', function() {
     searchBox.sendKeys('spike01');
     searchButton.click();
-    expect(element(by.binding('user.login')).getText()).toEqual('spike01')
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.getText()).toEqual(['spike01'])
   });
 
 
